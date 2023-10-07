69dcf58f1a859f3a1fb5e5cd919cc38d137e92c7
yhr0x43 <yhr0x43@gmail.com>
2023-10-07 18:10:26 -0500

    simple interact action with rope attach

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -19,9 +19,11 @@ public class PlayerControl : MonoBehaviour
     Vector2 moveDirection;
     Vector2 lookDirection;
 
-    public GameObject cameralFocal;
-    public GameObject virtualCamera;
+    public GameObject cameralFocal, virtualCamera;
     CharacterController controller;
+    PlayerStateManager stateManager;
+
+    public GameObject startObject, endObject;
 
     float yVelocity = 0f;
 
@@ -36,6 +38,7 @@ public class PlayerControl : MonoBehaviour
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        stateManager = GetComponent<PlayerStateManager>();
     }
 
     private void OnEnable()
@@ -58,7 +61,6 @@ public class PlayerControl : MonoBehaviour
         cameralFocal.transform.Rotate(Vector3.up * lookDirection.x * turnSpeed * Time.deltaTime);
         float cameraYaw = -virtualCamera.transform.rotation.y * Mathf.PI;
         Vector2 alignedMovement = Rotate(moveDirection, cameraYaw) * moveSpeed * Time.deltaTime;
-        Debug.Log(alignedMovement);
         if (!controller.isGrounded)
         {
             yVelocity -= gravity * Time.deltaTime;
@@ -78,13 +80,64 @@ public class PlayerControl : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
+        /* explain context.performed https://forum.unity.com/threads/player-input-component-triggering-events-multiple-times.851959/ */
+        if (!context.performed) return;
+
         /* TODO jumping via CharacterControlUtilities.StandardJump (requires KinematicCharacterBody?)
          * https://docs.unity3d.com/Packages/com.unity.charactercontroller@1.0/manual/jumping.html
          */
-        /* https://forum.unity.com/threads/player-input-component-triggering-events-multiple-times.851959/ */
-        if (controller.isGrounded && context.performed)
+        if (controller.isGrounded)
         {
             yVelocity = jumpDistance;
+        }
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        
+        Vector3[] lineIndexes;
+        float distanceFromIndex, shortestDistance = 10000;
+        Vector3 newPlayerPos = Vector3.zero;
+
+
+        RaycastHit hit;
+        // if the player and the startObject are within light of sight of each other
+        if (Physics.Raycast(transform.position, startObject.transform.position - transform.position, out hit, Vector3.Magnitude(startObject.transform.position - transform.position)))
+        {
+            Debug.Log(hit.collider.gameObject);
+
+            // get the number of line indexes created from line renderer
+            lineIndexes = new Vector3[startObject.GetComponent<LineRenderer>().positionCount];
+
+            // put all line indexes in a vector3 array
+            startObject.GetComponent<LineRenderer>().GetPositions(lineIndexes);
+
+            // find closet lineIndex to the player, and attach player to that lineIndex
+            for (int i = 0; i < lineIndexes.Length - 1; i++)
+            {
+                // find the closet lineIndex to the player
+                distanceFromIndex = Vector3.Distance(lineIndexes[i], transform.position);
+
+                //Debug.Log(distanceFromIndex);
+                // determine if the latest player/lineIndex distance is shorter than the previous one
+                if (distanceFromIndex < shortestDistance)
+                {
+                    newPlayerPos = lineIndexes[i];
+                    shortestDistance = distanceFromIndex;
+                }
+                //Debug.Log(lineIndexes[i]);
+            }
+
+            Debug.Log(newPlayerPos);
+            // update the player's transform to the nearest lineIndex
+            controller.enabled = false;
+            transform.position = newPlayerPos;
+            controller.enabled = true;
+        }
+        else
+        {
+            Debug.Log("Did not Hit");
         }
     }
 }
