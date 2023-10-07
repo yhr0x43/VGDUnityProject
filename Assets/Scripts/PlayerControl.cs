6eec9a67b5a102f006e6b34681db03486a47bc8c
yhr0x43 <yhr0x43@gmail.com>
2023-10-07 15:29:25 -0500

    movement with camera integrated

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -14,93 +14,78 @@ public class PlayerControl : MonoBehaviour
      * InputSystem: https://youtu.be/HmXU4dZbaMw
      * https://learn.unity.com/tutorial/configuring-an-xbox-controller-for-user-input-2019-2
      */
-    public InputMaster inputMaster;
     Vector2 moveDirection;
     Vector2 lookDirection;
-    InputAction moveInput;
-    InputAction lookInput;
-    InputAction jumpInput;
 
+    public GameObject cameralFocal;
+    public GameObject virtualCamera;
     Rigidbody rb;
-    bool touchedGround;
 
     /* https://discussions.unity.com/t/how-do-i-check-if-my-rigidbody-player-is-grounded/33250/11 */
-    private bool IsOnGround()
-    {
-        if (touchedGround)
-        {
-            touchedGround = false;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    bool isOnGround;
 
     private void OnCollisionEnter(Collision collision)
     {
         // NOTE consider downward facing collision as "touching ground" is this correct?
-        if (!touchedGround)
+        if (!isOnGround)
         {
-            touchedGround = (from contact in collision.contacts select contact.normal.y).Any(y => y > 0f);
+            isOnGround = (from contact in collision.contacts select contact.normal.y).Any(y => y > 0f);
         }
     }
 
     private void Awake()
     {
-        inputMaster = new InputMaster();
+
     }
 
     private void OnEnable()
     {
-        moveInput = inputMaster.Player.Move;
-        moveInput.Enable();
-
-        lookInput = inputMaster.Player.Look;
-        lookInput.Enable();
-
-        jumpInput = inputMaster.Player.Jump;
-        jumpInput.Enable();
-        jumpInput.performed += Jump;
 
     }
 
     private void OnDisable()
     {
-        moveInput.Disable();
-        lookInput.Disable();
-        jumpInput.Disable();
+
     }
 
-    // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        // TODO actually this should rotate camera
-        rb.transform.Rotate(Vector3.up * Time.deltaTime * lookDirection.x * turnSpeed);
+        cameralFocal.transform.Rotate(Vector3.up * Time.deltaTime * lookDirection.x * turnSpeed);
     }
 
     private void FixedUpdate()
     {
-        //Debug.Log($"{touchedGround}");
-        moveDirection = moveInput.ReadValue<Vector2>();
-        lookDirection = lookInput.ReadValue<Vector2>();
-        rb.AddRelativeForce(new Vector3(moveDirection.x, 0, moveDirection.y) * moveForce);
-        //rb.transform.Translate(new Vector3(moveDirection.x, 0, moveDirection.y) * Time.deltaTime * speed);
-        //rb.transform.Rotate(((Vector3.up * lookDirection.x) + (Vector3.left * lookDirection.y)) * Time.deltaTime * turnSpeed);
+        if (moveDirection.sqrMagnitude > 0.0625f)
+        {
+            /* FIXME jittering when moving while camera rotating */
+            float cameraYaw = virtualCamera.transform.rotation.y * 180;
+            Vector3 transformedMove = (Quaternion.Euler(0, cameraYaw, 0) * new Vector3(moveDirection.x, 0, moveDirection.y)).normalized;
+            rb.AddRelativeForce(transformedMove * moveForce);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookDirection = context.ReadValue<Vector2>();
     }
 
     /* https://gamedevbeginner.com/how-to-jump-in-unity-with-or-without-physics/ */
-    private void Jump(InputAction.CallbackContext context)
+    public void OnJump(InputAction.CallbackContext context)
     {
-        if (IsOnGround())
+        if (isOnGround)
         {
+            /* TODO consider OnCollisionExit */
+            isOnGround = false;
             rb.AddForce(Vector3.up * jumpForce);
         }
     }
