d92a466a6da7bf7d111002fd962c5e1b2907815c
yhr0x43 <yhr0x43@gmail.com>
2023-12-08 12:45:10 -0600

    pause function

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static bool isGamePaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PauseToggle()
+    {
+        isGamePaused = !isGamePaused;
+        if (isGamePaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public static void Pause()
+    {
+        isGamePaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public static void Unpause()
+    {
+        isGamePaused = false;
+        Time.timeScale = 1f;
+    }
+}
